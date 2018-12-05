c8fdc736bd829a4ab6660c5c3f5241999dff1733
naufalhaq <naufalhaq24@gmail.com>
2018-12-05 10:34:10 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Pesen Gambar</title>
+
+		<!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url()?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url()?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url()?>/assets/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url()?>/assets/css/creative.min.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<!-- navbar -->
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">Navbar</a>
+
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link disabled" href="#">Disabled</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+
+		<!-- colom -->
+		<section id="contact">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-lg-7 mx-auto text-left" style="padding: 100px; margin-top: 100px">
+	            <h2 class="section-heading">Desainer Favorit Anda dan Bekerjasama Dengan Mereka</h2>
+	            <h4 class="section-heading">Temukan Desainer Anda Sekarang!</h4>
+	            <div class="col-lg-4 mr-auto text-center" style="padding: 100px; margin-left: 30px">
+			        <a class="btn btn-dark btn-xl sr-button" href="#">Find Desainer</a>
+			      </div>
+	          </div>
+	          <div class="col-lg-5 mr-auto text-center">
+	            <p>
+	              <img src="<?php echo base_url()?>/assets/img/1.png">
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-lg-7 mx-auto text-left" style="padding-left:	100px;">
+	            <h2 class="section-heading">Posting Kebutuhan Anda dan Biarkan Desainer Memberikan Solusi</h2>
+	            <h4 class="section-heading">Kebutuhan Anda Ada Disini!</h4>
+	            <div class="col-lg-4 mr-auto text-center" style="padding: 100px; margin-left: 30px">	
+			      </div>
+	          </div>
+	          <div class="col-lg-5 mr-auto text-center">
+	            <p>
+	              <img src="<?php echo base_url()?>/assets/img/2.png">
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	    </section>
+
+
+		<!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url()?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url()?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="<?php echo base_url()?>/assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url()?>/assets/js/creative.min.js"></script>
+	</body>
+</html>
